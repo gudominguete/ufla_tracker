b02e0bc9d75d9b2ea1c686eb4722260f5bba8166
Gustavo Lopes <gudominguete@gmail.com>
2020-02-17 20:51:49 -0300

    Listagem de aparelhos

--- app/src/main/java/com/ufla/gustavo/uflatracker/ui/conexaobluetooth/ConexaoBluetoothActivity.kt
+++ app/src/main/java/com/ufla/gustavo/uflatracker/ui/conexaobluetooth/ConexaoBluetoothActivity.kt
@@ -2,29 +2,103 @@ package com.ufla.gustavo.uflatracker.ui.conexaobluetooth
 
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
 import android.content.Context
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.ufla.gustavo.uflatracker.R
 import android.content.Intent
 import android.content.IntentFilter
-import android.content.pm.PackageManager
 import android.os.Build
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_conexao_bluetooth.*
 
 
-class ConexaoBluetoothActivity : AppCompatActivity() {
+class ConexaoBluetoothActivity() : AppCompatActivity() {
 
-    private fun PackageManager.missingSystemFeature(name: String): Boolean = !hasSystemFeature(name)
+    var mBTDevices: ArrayList<BluetoothDevice> = ArrayList()
+    var mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_conexao_bluetooth)
+        setContentView(com.ufla.gustavo.uflatracker.R.layout.activity_conexao_bluetooth)
 
+
+        if (!mBluetoothAdapter.isEnabled()) {
+            val eintent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(eintent, 100)
+        }
+
+        discoverBluetooth()
     }
 
+    private fun discoverBluetooth(){
+        Log.d("teste", "btnDiscover: Looking for unpaired devices.")
+
+        if (mBluetoothAdapter.isDiscovering()) {
+            mBluetoothAdapter.cancelDiscovery()
+            Log.d("teste", "btnDiscover: Canceling discovery.")
+
+            //check BT permissions in manifest
+            checkBTPermissions()
+
+            mBluetoothAdapter.startDiscovery()
+            val discoverDevicesIntent = IntentFilter(BluetoothDevice.ACTION_FOUND)
+            registerReceiver(mBroadcastReceiver3, discoverDevicesIntent)
+        }
+        if (!mBluetoothAdapter.isDiscovering()) {
+
+            //check BT permissions in manifest
+            checkBTPermissions()
+
+            mBluetoothAdapter.startDiscovery()
+            val discoverDevicesIntent = IntentFilter(BluetoothDevice.ACTION_FOUND)
+            registerReceiver(mBroadcastReceiver3, discoverDevicesIntent)
+        }
+    }
+
+    private fun checkBTPermissions() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            var permissionCheck =
+                this.checkSelfPermission("Manifest.permission.ACCESS_FINE_LOCATION")
+            permissionCheck += this.checkSelfPermission("Manifest.permission.ACCESS_COARSE_LOCATION")
+            if (permissionCheck != 0) {
+
+                this.requestPermissions(
+                    arrayOf(
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    ), 1001
+                ) //Any number
+            }
+        } else {
+            Log.d(
+                "teste",
+                "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP."
+            )
+        }
+    }
+
+    private val mBroadcastReceiver3 = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action
+            Log.d("teste", "onReceive: ACTION FOUND.")
+
+            if (action == BluetoothDevice.ACTION_FOUND) {
+                val device =
+                    intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                mBTDevices.add(device)
+
+                Log.d("teste", "onReceive: " + device.name + ": " + device.address)
+                atualizarRecycleView()
+            }
+        }
+    }
+
+    private fun atualizarRecycleView() {
+        recycle_dispositivos.layoutManager = LinearLayoutManager(this)
+        var adapter = DispositivosAdapter(this, mBTDevices)
+        recycle_dispositivos.setAdapter(adapter);
+    }
 }
---
+++ app/src/main/java/com/ufla/gustavo/uflatracker/ui/conexaobluetooth/DispositivosAdapter.kt
@@ -0,0 +1,51 @@
+package com.ufla.gustavo.uflatracker.ui.conexaobluetooth
+
+import android.bluetooth.BluetoothClass
+import android.bluetooth.BluetoothDevice
+import android.content.Context
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.nio.file.Files.size
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ufla.gustavo.uflatracker.R
+
+
+class DispositivosAdapter(
+    internal var mctx: Context,
+    private val mList: List<BluetoothDevice>,
+    val funcao: (Unit) -> Unit
+) : RecyclerView.Adapter<DispositivosAdapter.DispositivosViewHolder>() {
+
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): DispositivosViewHolder {
+        val itemView = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.recycle_item_dispositivos, viewGroup, false)
+        return DispositivosViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(viewHolder: DispositivosViewHolder, i: Int) {
+        val dispositivo = mList[i]
+
+        viewHolder.viewNome.text = dispositivo.name
+        viewHolder.viewEndereco.text = dispositivo.address
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+
+    inner class DispositivosViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+
+        var viewNome: TextView
+        var viewEndereco: TextView
+
+        init {
+            viewNome = itemView.findViewById(R.id.conteudo_nome)
+            viewEndereco = itemView.findViewById(R.id.conteudo_endereco)
+        }
+    }
+}
